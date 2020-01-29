9713d26ba60dc5d6f6958ce6da97669fa1c1c8f3
Pavlo Grytsenko <pavlo@usf.edu>
2020-01-28 20:28:43 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PG_Iterative_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
